8523fe262cd01de7238d14a34d1d0afb64e75e03
unknown <josemarianoreyesh02@gmail.com>
2023-04-12 10:07:54 -0600

    cambios en mensajes

--- main.py
+++ main.py
@@ -30,7 +30,14 @@ class HBase:
             self.tables[table_name]["enabled"] = False
             return f"Tabla '{table_name}' deshabilitada."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
+
+    def enable(self, table_name):
+        if table_name in self.tables:
+            self.tables[table_name]["enabled"] = True
+            return f"Tabla '{table_name}' habilitada."
+        else:
+            return f"La tabla '{table_name}' no existe."
 
     def is_enabled(self, table_name):
         return self.tables.get(table_name, {}).get("enabled", False)
@@ -46,14 +53,14 @@ class HBase:
             else:
                 return f"La familia de columnas '{column_family}' no existe en la tabla '{table_name}'."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def drop(self, table_name):
         if table_name in self.tables:
             del self.tables[table_name]
             return f"Tabla '{table_name}' eliminada."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def drop_all(self):
         self.tables.clear()
@@ -63,7 +70,7 @@ class HBase:
         if table_name in self.tables:
             return self.tables[table_name]["column_families"]
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def put(self, table_name, row_key, column_family, column, value):
         if table_name in self.tables:
@@ -80,7 +87,7 @@ class HBase:
             else:
                 return f"La columna '{column_family}:{column}' no existe en la tabla '{table_name}'."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def get(self, table_name, row_key, column_family=None, column=None):
         if table_name in self.tables:
@@ -100,7 +107,7 @@ class HBase:
             else:
                 return f"El row_key '{row_key}' no existe en la tabla '{table_name}'."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def scan(
         self, table_name, start_row=None, end_row=None, column_family=None, column=None
@@ -130,7 +137,7 @@ class HBase:
                         return f"La columna '{column_family}:{column}' no existe en la tabla '{table_name}'."
             return scanned_rows
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def delete(self, table_name, row_key, column_family=None, column=None):
         if table_name in self.tables:
@@ -153,20 +160,20 @@ class HBase:
             else:
                 return f"El row_key '{row_key}' no existe en la tabla '{table_name}'."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def delete_all(self, table_name):
         if table_name in self.tables:
             self.tables[table_name]["rows"].clear()
             return f"Todos los datos en la tabla '{table_name}' han sido eliminados."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def count(self, table_name):
         if table_name in self.tables:
             return len(self.tables[table_name]["rows"])
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def truncate(self, table_name):
         if table_name in self.tables:
@@ -176,7 +183,7 @@ class HBase:
             self.create(table_name, column_families)
             return f"La tabla '{table_name}' ha sido truncada."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     # Puntos extra
     def update_many(self, table_name, data):
@@ -186,7 +193,7 @@ class HBase:
                     self.put(table_name, row_key, column_family, column, value)
             return f"Los datos de la tabla '{table_name}' se han actualizado."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
     def insert_many(self, table_name, data):
         if table_name in self.tables:
@@ -199,7 +206,7 @@ class HBase:
                                  column_family, column, value)
             return f"Los datos se han insertado en la tabla '{table_name}'."
         else:
-            return f"La tabla '{table_name}' no existe o está deshabilitada."
+            return f"La tabla '{table_name}' no existe."
 
 
 class HBaseGUI:
@@ -233,6 +240,9 @@ class HBaseGUI:
         elif tokens[0].lower() == "list_tables":
             return ", ".join(self.hbase.list_tables())
         elif tokens[0].lower() == "disable":
+            table_name = tokens[1]
+            return self.hbase.disable(table_name)
+        elif tokens[0].lower() == "enable":
             table_name = tokens[1]
             return self.hbase.disable(table_name)
         elif tokens[0].lower() == "is_enabled":
